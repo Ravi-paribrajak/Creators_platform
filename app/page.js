11820eac042659507d04450b6c4c62f2424bd902
Ravi Paribrajak <paribrajakravishankarkumar@gmail.com>
2026-01-06 14:33:21 +0530

    Set-up convex database

--- app/page.js
+++ app/page.js
@@ -4,7 +4,8 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <>
-      <Button variant={"destructive"}>Hello I am inside my creators_platform application</Button>
+      <Button >Hello I am inside my creators_platform application</Button>
+      <Button variant="primary">Hello</Button>
     </>
   );
 }
